126deae12b4cfac32433f4d43f98315bc629e518
dong_glb <980824550@qq.com>
2020-02-19 11:53:47 +0800

    消费者模块

---
+++ mochen-parent/mochen-consumer-dept-80/src/test/java/com/mochen/MochenConsumerDept80ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochen;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochenConsumerDept80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
