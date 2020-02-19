9449503a2b0be6040da76bced70da1d6452c1ba3
dong_glb <980824550@qq.com>
2020-02-19 12:01:13 +0800

    提供者模块

---
+++ mochen-parent/mochen-provider-dept-8001/src/main/java/com/mochen/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.mochen.service;
+
+import com.mochen.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService
+{
+	public boolean add(Dept dept);
+
+	public Dept get(Long id);
+
+	public List<Dept> list();
+}
---
+++ mochen-parent/mochen-provider-dept-8001/src/test/java/com/mochen/MochenProviderDept8001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochen;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochenProviderDept8001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
