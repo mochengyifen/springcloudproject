8b0045261b512aaa97345ce88484d7d9d69ae186
dong_glb <980824550@qq.com>
2020-02-19 11:58:43 +0800

    eureka服务注册中心

---
+++ mochen-parent/mochen-eureka-7002/src/main/java/com/mochen/MochenEureka7002Application.java
@@ -0,0 +1,15 @@
+package com.mochen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class MochenEureka7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MochenEureka7002Application.class, args);
+    }
+
+}
